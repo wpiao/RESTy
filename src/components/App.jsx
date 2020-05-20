68f0f7edd56e463c0b9a688c9d75356f20273fb8
Wenhao Piao <wenhaopiao520@gmail.com>
2020-05-20 14:32:52 -0700

    Initial Env Setup

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // add some state
+    };
+  }
+
+  // add some functions
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>Hello, World!</div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+const Main = () => <App />
+
+ReactDOM.render(<Main />, document.getElementById('root'));

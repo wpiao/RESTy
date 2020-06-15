9a95f7a768d3c73f04257afea374d5b20c16c375
Wenhao Piao <wenhaopiao520@gmail.com>
2020-06-15 14:16:56 -0700

    Updates

---
+++ precache-manifest.9abb780ee5e7d3ab54e031a142eb2f4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5c5683165d00341d9ea557f1124e03",
+    "url": "/github.io/RESTy/index.html"
+  },
+  {
+    "revision": "4edee4ffb1288e74df0e",
+    "url": "/github.io/RESTy/static/js/2.84672c6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/RESTy/static/js/2.84672c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b9d6f02f9799b46071",
+    "url": "/github.io/RESTy/static/js/main.fe2864cd.chunk.js"
+  },
+  {
+    "revision": "55e5b42162f3efd34ceb",
+    "url": "/github.io/RESTy/static/js/runtime-main.016e3be2.js"
+  }
+]);
